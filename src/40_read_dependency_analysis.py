f06dd4926a0825994e847a572a73a62d54429c3a
MakMemEng <yatani.mems4568@gmail.com>
2023-09-07 23:39:36 +0000

    ♻️ Ruffを適用してリファクタリング

--- src/40_read_dependency_analysis.py
+++ src/40_read_dependency_analysis.py
@@ -1,5 +1,3 @@
-import Cabocha
-
 class Morph:
     def __init__(self, surface, base, pos, pos1):
         self.surface = surface
@@ -10,21 +8,22 @@ class Morph:
     def show(self):
         print(self.surface, self.base, self.pos, self.pos1)
 
-path = 'neko.txt.cabocha'
+
+path = "neko.txt.cabocha"
 with open(path) as f:
-    text = f.read().split('\n')
+    text = f.read().split("\n")
 result = []
 morphs = []
-for line in test[:-1]:
-    if line == 'EOS':
+for line in text[:-1]:
+    if line == "EOS":
         result.append(morphs)
         morphs = []
-    elif line[0] == '*':
+    elif line[0] == "*":
         continue
     else:
-        ls = line.split('\t')
+        ls = line.split("\t")
         d = {}
-        tmp = ls[1].split(',')
+        tmp = ls[1].split(",")
         morph = Morph(ls[0], tmp[6], tmp[0], tmp[1])
         morph.append(morph)
 for morphs in result[2]:
--- src/41_read_dependecy_analysis.py
+++ src/41_read_dependecy_analysis.py
@@ -4,7 +4,7 @@ class Morph:
         self.base = base
         self.pos = pos
         self.pos1 = pos1
-    
+
     def show(self):
         print(self.surface, self.base, self.pos, self.pos1)
 
@@ -15,30 +15,32 @@ class Chunk:
         self.chunk_id = chunk_id
         self.dst = dst
         self.srcs = srcs
+
     def show_morphs(self):
-        morphs = ''
+        morphs = ""
         for morph in self.morphs:
             morphs += morph.surface
         print("morphs:", morphs)
-    
+
     def show_chunk_id(self):
         print("===========")
         print("chunk_id:", self.chunk_id)
-    
+
     def show_sentense_id(self):
-        if self.chunk_id is 0:
+        if self.chunk_id == 0:
             print("===========")
             print("sentence_id", self.sentence_id)
-    
+
     def show_dst(self):
         print("dst:", self.dst)
-    
+
     def show_srcs(self):
         print("srcs:", self.srcs[self.chunk_id])
 
-path = 'neko.txt.cabocha'
+
+path = "neko.txt.cabocha"
 with open(path) as f:
-    text = f.read().split('\n')
+    text = f.read().split("\n")
 result = []
 morphs = []
 chunks = []
@@ -48,13 +50,13 @@ sentence_id = 0
 chunk_id = 0
 
 for line in text[:-1]:
-    if line is 'EOS':  # todo if line == 'EOS': との比較
+    if line == "EOS":  # todo if line == 'EOS': との比較
         result.append(morphs)
         morphs = []
         sentence_id += 1
         chunk_id = 0
-        srcs = [[]] 
-    elif line[0] is '*':  # todo if line == '*': との比較
+        srcs = [[]]
+    elif line[0] == "*":  # todo if line == '*': との比較
         if chunk:
             chunks.append(chunk)
         dst = int(line.split()[2][:-1])
@@ -66,19 +68,17 @@ for line in text[:-1]:
         chunk = Chunk(sentence_id, chunk_id, dst, srcs)
         chunk_id += 1
     else:
-        ls = line.split('\t')
+        ls = line.split("\t")
         d = {}
-        tmp = ls[1].split(',')
+        tmp = ls[1].split(",")
         morph = Morph(ls[0], tmp[6], tmp[0], tmp[1])
         morphs.append(morph)
         chunk.morphs.append(morph)
-else:
     chunks.append(chunk)
-for i, chunk in enumerate(chunks):
-    if chunk.sentence_id is 7:  # todo if chunk.sentence_id == 7: PatternMatch との比較
+for _, chunk in enumerate(chunks):
+    if chunk.sentence_id == 7:
         chunk.show_sentense_id()
         chunk.show_chunk_id()
         chunk.show_dst()
         chunk.show_srcs()
         chunk.show_morphs()
-            
